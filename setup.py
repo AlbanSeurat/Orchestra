15589a95fb4f3cb0ea212ad7c22c6e36fbe78744
Alban Seurat <alban.seurat@me.com>
2013-08-19 03:24:18 -0700

    buildout build

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
     packages = find_packages("src"),
     package_dir={"": "src"},
     data_files=[ ('', ['src/__main__.py', 'src/manage_movie.py' ]) ],
-    
+    install_requires=['setuptools', 'putio.py', "requests", "iso8601" ],
+    namespace_packages=['orchestra',],
+    py_modules=['orchestra'],
+
     author= "Alkpone",
     author_email = "alkpone@alkpone.com",
     url = "https://github.com/Alkpone/Orchestra"
